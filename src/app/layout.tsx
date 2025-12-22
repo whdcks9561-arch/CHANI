4faac22410183694953c76c20bedeef06b31299b
whdcks9561-arch <whdcks9561@gmail.com>
2025-12-23 08:25:43 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import "./globals.css";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="bg-slate-950 text-white">
+        {children}
+      </body>
     </html>
   );
 }
