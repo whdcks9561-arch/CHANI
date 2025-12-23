c730496b2c0f53dd17ad30adbed14ddf867a62c4
whdcks9561-arch <whdcks9561@gmail.com>
2025-12-23 09:41:42 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import Camera from "../components/Camera";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 style={{ color: "red", fontSize: 24 }}>
-        Camera 테스트 페이지
-      </h1>
-      <Camera />
-    </div>
-  );
+  return <Camera />;
 }
